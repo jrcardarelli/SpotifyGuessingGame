5fb36db2e9c803f7ebcd913f0724ef330b264c1b
jrcardarelli <joecardarelli1077@gmail.com>
2024-01-13 18:04:54 -0500

    more spotify data

--- json_parsing.py
+++ json_parsing.py
@@ -5,6 +5,8 @@ def collect_recent_tracks(json_data):
     all_recent_song_names = [item.get('track', {}).get('name') for item in json_data.get('items', [])]
     all_recent_song_previews = [item.get('track', {}).get('preview_url') for item in json_data.get('items', [])]
     all_recent_song_duration = [item.get('track', {}).get('duration_ms') for item in json_data.get('items', [])]
+    all_recent_song_covers = [item.get('track', {}).get('album', {}).get('images', [{}])[0].get('url') for item in json_data.get('items', [])]
+    all_recent_song_date = [item.get('track', {}).get('release_date') for item in json_data.get('items', [])]
 
     res = {}
     for key in all_recent_song_names:
@@ -20,6 +22,20 @@ def collect_recent_tracks(json_data):
             all_recent_song_duration.remove(value)
             break
 
+    res3 = {}
+    for key in all_recent_song_names:
+        for value in all_recent_song_covers:
+            res2[key] = value
+            all_recent_song_covers.remove(value)
+            break
+
+    res4 = {}
+    for key in all_recent_song_names:
+        for value in all_recent_song_date:
+            res2[key] = value
+            all_recent_song_date.remove(value)
+            break
+
     all_recent_song_names = list(set(all_recent_song_names))
 
     random_songs = random.sample(all_recent_song_names, 10)
@@ -32,10 +48,20 @@ def collect_recent_tracks(json_data):
     for song in random_songs:
         result_duration.append(res2.get(song))
 
+    result_covers = []
+    for song in random_songs:
+        result_covers.append(res3.get(song))
+
+    result_date = []
+    for song in random_songs:
+        result_date.append(res3.get(song))
+
     return {
         "names": random_songs,
         "previews": result_previews,
         "durations": result_duration,
+        "covers": result_covers,
+        "dates": result_date,
     }
 
 
